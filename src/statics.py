1d6e3f311da1b7a75bce5e9ef76f124453e4dddd
antek <balcerski.antoni@gmail.com>
2021-04-30 14:56:35 +0200

    initit

---
+++ src/statics.py
@@ -0,0 +1,17 @@
+WIDTH = 1900
+HEIGHT = 1000
+FPS = 60
+PUNKTY = 0
+
+LEFT = 'LEFT'
+RIGHT = 'RIGHT'
+UP = 'UP'
+DOWN = 'DOWN'
+
+# COLORS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 247, 0)
